d87a4b652fc2c709b036d0eaf263bd4c917bf089
gingermadfire <Maxim.ilian98@yandex.ru>
2023-04-16 20:18:23 +0300

    Initial commit

---
+++ src/main/java/ru/redsoft/warships/WarshipsApplication.java
@@ -0,0 +1,13 @@
+package ru.redsoft.warships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarshipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/redsoft/warships/WarshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.redsoft.warships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarshipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
